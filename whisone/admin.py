243984ea8544ef9fa14eaf835f74fe5a0fa2bfdc
abednego emonena <emonenaabednego@gmail.com>
2025-11-16 18:11:41 +0100

    feat: Add KnowledgeVaultEntry and UserPreference admin interfaces with JSON display features

--- whisone/admin.py
+++ whisone/admin.py
@@ -57,3 +57,43 @@ class AutomationRuleAdmin(admin.ModelAdmin):
             "fields": ("created_at", "updated_at")
         }),
     )
+
+
+from django.contrib import admin
+from .models import KnowledgeVaultEntry, UserPreference
+import json
+from django.utils.html import format_html
+
+@admin.register(KnowledgeVaultEntry)
+class KnowledgeVaultEntryAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "memory_id", "summary_snippet", "timestamp", "last_accessed")
+    search_fields = ("memory_id", "summary", "user__username", "user__email")
+    list_filter = ("timestamp", "last_accessed", "user")
+    readonly_fields = ("timestamp", "last_accessed")
+
+    def summary_snippet(self, obj):
+        """Show a short snippet of the summary"""
+        return (obj.summary[:75] + "...") if obj.summary else "-"
+    summary_snippet.short_description = "Summary"
+
+    def entities_json(self, obj):
+        """Pretty-print JSON for entities"""
+        return format_html("<pre>{}</pre>", json.dumps(obj.entities, indent=2))
+    entities_json.short_description = "Entities"
+
+    def preferences_json(self, obj):
+        """Pretty-print JSON for preferences"""
+        return format_html("<pre>{}</pre>", json.dumps(obj.preferences, indent=2))
+    preferences_json.short_description = "Preferences"
+
+@admin.register(UserPreference)
+class UserPreferenceAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "preferences_snippet", "last_updated")
+    search_fields = ("user__username", "user__email")
+    readonly_fields = ("last_updated",)
+
+    def preferences_snippet(self, obj):
+        """Show a short snippet of the preferences JSON"""
+        prefs_str = json.dumps(obj.preferences, indent=2)
+        return format_html("<pre>{}</pre>", prefs_str)
+    preferences_snippet.short_description = "Preferences"
