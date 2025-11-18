c88195d2672e0017349545195aeb13dd78885413
abednego emonena <emonenaabednego@gmail.com>
2025-11-18 10:47:25 +0100

    fix: Update daily summary task schedule to run at 10:49 AM

--- whisprai/production.py
+++ whisprai/production.py
@@ -465,7 +465,7 @@ CELERY_BEAT_SCHEDULE = {
     },
     "daily-summary-9am": {
         "task": "whisone.tasks.daily_summary.run_daily_summary",
-        "schedule": crontab(hour=10, minute=47),  # every day at 8 AM
+        "schedule": crontab(hour=10, minute=49),  # every day at 8 AM
     }
 }
 
