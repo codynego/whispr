9adf6c0ad210712dc45737273c11b4dfc35cbed1
abednego emonena <emonenaabednego@gmail.com>
2025-12-29 18:55:56 +0100

    fix: update daily summary task schedule to run at 6:57 PM

--- whisprai/celery.py
+++ whisprai/celery.py
@@ -49,7 +49,7 @@ app.conf.beat_schedule = {
     },
     "daily-summary-9am": {
         "task": "whisone.tasks.daily_summary.run_daily_summary",
-        "schedule": crontab(hour=18, minute=55),  # every day at 5:45 PM
+        "schedule": crontab(hour=18, minute=57),  # every day at 5:45 PM
     }
 }
 
